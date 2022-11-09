23f4f3733ce4cd9419843c784126241a21cdb745
Rafael vetrano <rafaelcairo2010@hotmail.com>
2022-11-09 17:44:15 -0300

    req12

---
+++ challenges/desafio12.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+    {
+        $and: [
+        { ingredientes: { $ne: "ketchup" } },
+        { nome: { $ne: "McChicken" } },
+        ],
+    },
+    {
+        $push: {
+            ingredientes: "ketchup",
+        },
+    },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
