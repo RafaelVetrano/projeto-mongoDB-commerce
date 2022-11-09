69ed90ad2fcad22b0645b6e0d900b8eb096b7a8b
Rafael vetrano <rafaelcairo2010@hotmail.com>
2022-11-09 15:43:50 -0300

    req 7. line length updated

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,2 @@
-db.produtos.find({ vendidos: { $ne: 50 }, tags: { $exists: false } }, { nome: 1, vendidos: 1, _id: 0 });
+db.produtos
+.find({ vendidos: { $ne: 50 }, tags: { $exists: false } }, { nome: 1, vendidos: 1, _id: 0 });
